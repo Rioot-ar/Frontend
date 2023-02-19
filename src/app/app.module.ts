87be9c8079007ee44a8218ff8f626478ee7e9edb
Rioot-Ar <gaston318@live.com>
2023-02-19 18:01:34 -0300

    errorpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { NavigatorComponent } from './components/navigator/navigator.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
-import { InfoItemComponent } from './components/info-item/info-item.component';
 import { HardSoftSkillsComponent } from './components/hard-soft-skills/hard-soft-skills.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -25,7 +24,6 @@ import { SeccionComponent } from './components/seccion/seccion.component';
     NavigatorComponent,
     BannerComponent,
     AcercaDeComponent,
-    InfoItemComponent,
     HardSoftSkillsComponent,
     ProyectosComponent,
     GraficosComponent,
